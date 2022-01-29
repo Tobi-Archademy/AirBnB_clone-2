4f589c8a5b3e4088a938acc54d84fe6ae73bff98
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-01-28 18:20:02 -0800

    hello flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+strict_slashes=False
+
+
+@app.route("/")
+def hello():
+    """return the root page"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
