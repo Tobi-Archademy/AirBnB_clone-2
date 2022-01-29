cea0998682a4bba24c901bab973bdf3206ec5e34
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-01-28 19:16:01 -0800

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+"""
+
+from flask import Flask
+app = Flask(__name__)
+strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """ root routing """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ extension hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<arg>")
+def cisfun(arg):
+    """ using dynamic url """
+    string = "C " + arg
+    return string.replace("_", " ")
+
+
+@app.route("/python/")
+@app.route("/python")
+def hello1():
+    """ extension /python """
+    return "Python is cool"
+
+
+@app.route("/python/<arg>")
+def hello2(arg):
+    """ python extension /text """
+    string = "Python " + arg
+    return string.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
