75b1a4f3b55dbded6e1548811ea167d92d800f5e
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-01-28 19:00:20 -0800

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+"""
+
+from flask import Flask
+app = Flask(__name__)
+strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """ root routing """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ extension hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<arg>")
+def cisfun(arg):
+    """ using dynamic url """
+    string = "C " + arg
+    return string.replace("_", " ")
+    
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
