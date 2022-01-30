885373aededdd63349a31089d7fd722ec4d7bf1c
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-01-30 10:07:48 -0800

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,8 @@ Write a script that starts a Flask web application:
 
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
 strict_slashes = False
 
